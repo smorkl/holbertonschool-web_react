8b24eeaa62d5bdacae765c89892feea74788207b
smorkl <smsalazar0319@gmail.com>
2025-02-02 23:51:27 -0500

    cereat

---
+++ TypeScript/task5/js/main.ts
@@ -0,0 +1,33 @@
+// task_5/js/main.ts
+
+interface MajorCredits {
+	credits: number;
+	brand: "MajorCredits";
+}
+  
+interface MinorCredits {
+	credits: number;
+	brand: "MinorCredits";
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+	return {
+	  credits: subject1.credits + subject2.credits,
+	  brand: "MajorCredits",
+	};
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+	return {
+	  credits: subject1.credits + subject2.credits,
+	  brand: "MinorCredits",
+	};
+}
+
+const major1: MajorCredits = { credits: 3, brand: "MajorCredits" };
+const major2: MajorCredits = { credits: 4, brand: "MajorCredits" };
+const minor1: MinorCredits = { credits: 2, brand: "MinorCredits" };
+const minor2: MinorCredits = { credits: 1, brand: "MinorCredits" };
+  
+console.log(sumMajorCredits(major1, major2));
+console.log(sumMinorCredits(minor1, minor2));
